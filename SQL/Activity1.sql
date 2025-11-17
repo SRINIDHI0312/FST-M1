db78b5ff492c8f9d2fae9afc8c9133bd73350057
SRINIDHI0312 <srinidhi.mur3@gmail.com>
2025-11-17 19:50:51 +0530

    Add files via upload

---
+++ SQL/Activity1.sql
@@ -0,0 +1,4 @@
+	
+CREATE TABLE salesman(salesman_id int PRIMARY KEY, salesman_name varchar(32), salesman_city varchar(32), commission int );
+
+DESCRIBE salesman;
---
+++ SQL/Activity5.sql
@@ -0,0 +1,8 @@
+
+--Activity-5
+--Update the grade score of salesmen from Rome to 200.
+Update salesman SET grade = 200 where salesman_city = 'Rome';
+--Update the grade score of James Hoog to 300.
+Update salesman SET grade = 300 where salesman_name = 'Pit Alex';
+--Update the name McLyon to Pierre.
+Update salesman SET salesman_name = 'Pierre' WHERE salesman_name = 'McLyon'
