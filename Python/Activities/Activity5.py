302bd0e2c29d841b728237d4cbafa96453a296cf
SRINIDHI0312 <srinidhi.mur3@gmail.com>
2025-11-17 19:47:41 +0530

    Add files via upload

---
+++ Python/Activities/Activity5.py
@@ -0,0 +1,7 @@
+# Ask the user to enter a number
+num = int(input("Enter a number to print its multiplication table: "))
+
+# Print the multiplication table from 1 to 10
+print(f"\nMultiplication Table of {num}:")
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
