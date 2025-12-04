64b6b7b57ef772bf830e5f4ff0a8452ed46b3df3
SRINIDHI0312 <srinidhi.mur3@gmail.com>
2025-12-04 14:28:22 +0530

    Update Activity7.java

--- Selenium/Project/Activity7.java
+++ Selenium/Project/Activity7.java
@@ -1,20 +1,80 @@
-@Test
-	public void Activity6() throws InterruptedException {
-		driver.get("https://alchemy.hguy.co/jobs");
-		Job = driver.findElement(By.xpath("//*[@class=\"main-navigation\"]//li[1]/a"));
-		Job.click();
-		Keyword = driver.findElement(By.id("search_keywords"));
-		Keyword.sendKeys("Banking");
-		Search_jobs = driver.findElement(By.xpath("//*[@class='search_submit']/input"));
-		Search_jobs.click();
-		Thread.sleep(2000);
-		Job_listing = driver.findElements(By.xpath("//ul[@class='job_listings']/li"));
-		Job_listing.get(0).click();
-		Apply_jobs = driver.findElement(By.xpath("//*[@class='single_job_listing']//input"));
-		Apply_jobs.click();
-		Thread.sleep(2000);
-		Job_application_email = driver.findElement(By.className("job_application_email"));
-		String email = Job_application_email.getText();
-		assertEquals(email, "abhiram@cklabs.com");
-		driver.close();
+
+package SeleniumProject;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Activity7 {
+	WebDriver driver;
+	WebDriverWait wait;
+	@BeforeClass
+	public void setUp() {
+		driver = new ChromeDriver();
+		wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+		driver.get("https://alchemy.hguy.co/jobs/");
+		driver.manage().window().maximize();
 	}
+	@Test
+	public void createJobListingTest() {
+		WebElement jobPost = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//li[contains(@id,'menu-item')]//a[text()='Post a Job']")));
+		//driver.findElement(By.tagName("//li[contains(@id,'menu-item')]//a[text()='Jobs']"));
+		jobPost.click();
+		WebElement previewButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@value='Preview']")));
+		//driver.findElement(By.tagName("//li[contains(@id,'menu-item')]//a[text()='Jobs']"));
+		previewButton.click();
+		driver.findElement(By.id("create_account_email")).sendKeys("svtest121@gmail.com");
+		driver.findElement(By.id("job_title")).sendKeys("Automation Engineer 1");
+		driver.findElement(By.id("job_location")).sendKeys("bangalore");
+		WebElement jobType=driver.findElement(By.id("job_type"));
+		Select select=new Select(jobType);
+		select.selectByVisibleText("Full Time");
+		WebElement iframeElement=driver.findElement(By.id("job_description_ifr"));
+		driver.switchTo().frame(iframeElement);
+		//tinymce
+		driver.findElement(By.id("tinymce")).sendKeys("applying for job");
+		driver.switchTo().defaultContent();
+		driver.findElement(By.id("application")).sendKeys("https://alchemy.hguy.co/jobs/");
+		
+		driver.findElement(By.id("company_name")).sendKeys("tcre");
+		driver.findElement(By.id("company_website")).sendKeys("https://alchemy.hguy.co/jobs/");
+		previewButton.click();
+		
+		WebElement submitListingButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("job_preview_submit_button")));
+		//driver.findElement(By.tagName("//li[contains(@id,'menu-item')]//a[text()='Jobs']"));
+		submitListingButton.click();
+		
+		
+	}
+
+	@Test
+	public void jobPostTest() {
+		WebElement heading = driver.findElement(By.tagName("h1"));
+		String headingText = heading.getText();
+		System.out.println("Webpage Heading: " + headingText);
+		Assert.assertEquals(headingText, "Jobs");
+	}
+	@Test
+	public void jobListingTest() {
+		WebElement jobLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//li[contains(@id,'menu-item')]//a[text()='Jobs']")));
+		//driver.findElement(By.tagName("//li[contains(@id,'menu-item')]//a[text()='Jobs']"));
+		jobLink.click();	
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='position']/h3[text()='Automation Engineer 1']")));
+	
+	}
+	
+	@AfterClass
+	public void TearDown() {
+		//driver.close();
+	}
+}
